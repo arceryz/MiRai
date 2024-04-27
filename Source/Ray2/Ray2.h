791537e901d6e180290fedb63b300f10da38c8b2
arceryz <t.j.m.vandervalk@proton.me>
2024-04-27 11:24:55 +0200

    Seperate Ray2 to seperate UI file

---
+++ Source/Ray2/Ray2.h
@@ -0,0 +1,71 @@
+#ifndef RAY2_H
+#define RAY2_H
+
+#include "Ray2Scene.h"
+#include "Ray2Program.h"
+#include "raygui.h"
+
+// This class contains the Ray2 running/UI code.
+class Ray2 
+{
+private:
+    Ray2Scene scene;
+    Ray2Program program;
+
+    float numRaysFact = 0.5;
+    float focusFact = 1.0;
+    bool hyperbolic = false;
+    int shape = 5;
+    int currentShape = 5;
+    Vector3 color = { 1.0, 1.0, 1.0 };
+    Camera2D camera = {};
+public:
+    Ray2(): program(scene)
+    {
+        scene.GenerateRegularPolygon(5);
+        program.numRays = 1024;
+        program.numBounces = 4;
+        camera.offset = { 400, 400 };
+        camera.target = { 400, 400 };
+        camera.zoom = 1.0;
+    };
+    void Draw() 
+    {
+        program.numRays = (int)(pow(numRaysFact, 8) * MAX_RAYS);
+        program.color = ColorFromHSV(color.x, color.y, color.z);
+        program.arcFocus = (hyperbolic ? -1 : 1) * (1+pow(focusFact, 5) * (ARC_FOCUS_INF-1));
+
+        BeginMode2D(camera);
+        program.ComputePass();
+        program.RenderPass();
+        EndMode2D();
+
+        DrawText(TextFormat("N=%d", program.numRays), 10, 30, 20, DARKGRAY);
+        DrawFPS(10, 10);
+
+        float ox = 170;
+        float oy = 15;
+        GuiSlider({ ox, oy, 200, 10 }, "Rays%", TextFormat("%3.1f", 100*numRaysFact), &numRaysFact, 0, 1);
+        GuiSlider({ ox, oy+20, 200, 10 }, "Focus", TextFormat("%.2f", program.arcFocus), &focusFact, 0, 1);
+        GuiSlider({ ox+270, oy, 100, 10 }, "Zoom", TextFormat("%.1f", camera.zoom), &camera.zoom, 0.1, 2);
+        GuiCheckBox({ ox+240, oy+15, 20, 20 }, "Hyperbolic", &hyperbolic);
+        GuiSpinner({ ox, oy+40, 80, 15 }, "Num Bounces ", &program.numBounces, 1, MAX_BOUNCES, false);
+        GuiSpinner({ ox+130, oy+40, 80, 15 }, "Shape ", &shape, 3, 10, false);
+    
+        ox = 660;
+        oy = 13;
+        GuiColorPickerHSV({ ox, oy, 100, 70 }, "Color", &color);
+        GuiSlider({ ox, oy+80, 100, 10 }, "Falloff", TextFormat("%.2f", program.falloff), &program.falloff, 0.001, 1);
+        GuiSlider({ ox, oy+100, 100, 10 }, "Point Size", TextFormat("%.2f", program.pointSize), &program.pointSize, 0.3, 3);
+
+        if (currentShape != shape) {
+            currentShape = shape;
+            scene.GenerateRegularPolygon(shape);
+        }
+        if (IsKeyPressed(KEY_R)) {
+            scene.GenerateRandomCirclePolygon(5, 20);
+        }
+    }
+};
+
+#endif
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,7 @@
 #include <raylib.h>
 #include <raymath.h>
 #include <rlgl.h>
-#include "Ray2Scene.h"
-#include "Ray2Program.h"
+#include "Ray2.h"
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
@@ -13,65 +12,13 @@
 int main()
 {
     InitWindow(800, 800, "Mirai R");
-    //SetTargetFPS(144);
-
-    Ray2Scene scene;
-    scene.GenerateRegularPolygon(5);
-
-    Ray2Program ray2(scene);
-    ray2.numRays = 1024;
-    ray2.numBounces = 4;
-
-    float numRaysFact = 0.5;
-    float focusFact = 1.0;
-    bool hyperbolic = false;
-    int shape = 5;
-    int currentShape = 5;
-    Vector3 color = { 1.0, 1.0, 1.0 };
-
-    Camera2D camera = {};
-    camera.offset = { 400, 400 };
-    camera.target = { 400, 400 };
-    camera.zoom = 1.0;
-
+    Ray2 ray2;
+    
     while(!WindowShouldClose()) {
-        ray2.numRays = (int)(pow(numRaysFact, 8) * MAX_RAYS);
-        ray2.color = ColorFromHSV(color.x, color.y, color.z);
-        ray2.arcFocus = (hyperbolic ? -1 : 1) * (1+pow(focusFact, 5) * (ARC_FOCUS_INF-1));
-
         BeginDrawing();
         ClearBackground(BLACK); 
 
-        BeginMode2D(camera);
-        ray2.ComputePass();
-        ray2.RenderPass();
-        EndMode2D();
-
-        DrawText(TextFormat("N=%d", ray2.numRays), 10, 30, 20, DARKGRAY);
-        DrawFPS(10, 10);
-
-        float ox = 170;
-        float oy = 15;
-        GuiSlider({ ox, oy, 200, 10 }, "Rays%", TextFormat("%3.1f", 100*numRaysFact), &numRaysFact, 0, 1);
-        GuiSlider({ ox, oy+20, 200, 10 }, "Focus", TextFormat("%.2f", ray2.arcFocus), &focusFact, 0, 1);
-        GuiSlider({ ox+270, oy, 100, 10 }, "Zoom", TextFormat("%.1f", camera.zoom), &camera.zoom, 0.1, 2);
-        GuiCheckBox({ ox+240, oy+15, 20, 20 }, "Hyperbolic", &hyperbolic);
-        GuiSpinner({ ox, oy+40, 80, 15 }, "Num Bounces ", &ray2.numBounces, 1, MAX_BOUNCES, false);
-        GuiSpinner({ ox+130, oy+40, 80, 15 }, "Shape ", &shape, 3, 10, false);
-    
-        ox = 660;
-        oy = 13;
-        GuiColorPickerHSV({ ox, oy, 100, 70 }, "Color", &color);
-        GuiSlider({ ox, oy+80, 100, 10 }, "Falloff", TextFormat("%.2f", ray2.falloff), &ray2.falloff, 0.001, 1);
-        GuiSlider({ ox, oy+100, 100, 10 }, "Point Size", TextFormat("%.2f", ray2.pointSize), &ray2.pointSize, 0.3, 3);
-
-        if (currentShape != shape) {
-            currentShape = shape;
-            scene.GenerateRegularPolygon(shape);
-        }
-        if (IsKeyPressed(KEY_R)) {
-            scene.GenerateRandomCirclePolygon(5, 20);
-        }
+        ray2.Draw();
 
         EndDrawing();
     }
